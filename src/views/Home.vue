2f9cd467c634cc8de926842624618573750fe3a8
Makoto Ogata <ogatamakoto1976@gmail.com>
2019-11-08 22:16:15 +0900

    chapter 2-5 is done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="list">
+      <li v-for="whisper in orderBy(whispers,'date',-1)" :key="whisper.id" class="item">
+        <div class="user-box">
+          <div class="avatar" :style="'background-image: url('+url+')'"></div>
+          <p class="user-name">{{ whisper.uid }}</p>
+        </div>
+        <div class="content" v-html="whisper.content"></div>
+      </li>
+    </div> 
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import { db } from '../main'
+import Vue2Filters from 'vue2-filters' 
 
 export default {
   name: 'home',
+  data() {
+    return {
+      whispers: []
+    }
+  },
+  firestore() {
+    return {
+      whispers: db.collection('whispers')
+    }
+  },
   components: {
-    HelloWorld
-  }
+  },
+
+  mixins: [Vue2Filters.mixin] 
 }
 </script>
+
+<style lang="stylus" scoped>
+.item
+  list-style none
+  border-top 1px solid #eee
+  padding 5px 15px
+  display flex
+  flex-wrap no-wrap
+  justify-content flex-start
+  position relative
+  &:first-child
+    border none
+  &:hover
+    background rgba(0,0,0,.02)
+  .user-box
+    margin 10px 10px 10px 0
+    .avatar
+      height 50px
+      width 50px
+      border-radius 50%
+      border 1px solid #eee
+      background-size cover
+    .user-name
+      margin 5px 0 0 0
+      text-align center
+      font-size .7rem
+      line-height .7rem
+      width 50px
+  .content
+    padding 10px
+</style>
