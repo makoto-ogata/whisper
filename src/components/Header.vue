85c23b5e203f63e3c2275cd1313b9f8e6bfbcc4c
Makoto Ogata <ogatamakoto1976@gmail.com>
2019-11-10 23:32:34 +0900

    chapter 2-6 and 3-1 is done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,12 +5,39 @@
       <h1>whisp.</h1>
     </router-link>
     <div class="btns">
-      <button>
-        <fa icon="user" />
+      <button @click="signIn">
+        <fa icon="user"/>
       </button>
     </div>
   </header>
 </template>
+
+<script>
+import firebase from 'firebase'
+import { auth } from '../main'
+
+export default {
+  methods: {
+    signIn () {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      auth.signInWithPopup(provider)
+      .then((result) => {
+        alert('Hello, '+result.user.displayName+'!')
+      })
+    },
+    signOut () {
+      if (window.confirm('Are You Sure to Sign Out?')) {
+        auth.signOut()
+        .then(() => {
+          alert('You Safely Signed Out.')
+          this.$router.push('/'),
+          location.reload()
+        })
+      }
+    }
+  }
+}
+</script>
 
 <style lang="stylus" scoped>
 header
